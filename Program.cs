9da2aee99910a5e8dda6d262157199d9281ccfdc
LeandroBelge <leandrobelge@gmail.com>
2021-07-31 11:57:20 -0300

    Removendo moeda e realizando operação

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Regex_dotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string moeda1 = "R$9.999,50";
+            string moeda2 = "R$8.999,00";
+            //remove a moeda
+            string reg = @"(^R)(\$)";
+            moeda1 = Regex.Replace(moeda1, reg, "");
+            moeda2 = Regex.Replace(moeda2, reg, "");
+            //Converte e realiza a operação
+            double valor = Convert.ToDouble(moeda1) - Convert.ToDouble(moeda2);
+            //Formata a moeda de acordo com a máquina.
+            Console.WriteLine(valor.ToString("C2", CultureInfo.CurrentCulture));
+        }
+    }
+}
